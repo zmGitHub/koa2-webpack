e3a8552afc9e99ec8b055efa8c76e36228814204
guowen <guowentao@terminus.io>
2017-02-28 22:35:21 +0800

    add sportsman public

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,9 @@
 import CoreLayout from './CoreLayout';
 import Home from './Home';
 import loginRoute from './Login';
+import sportsmanRouter from './Sportsman';
+
+// 运动员管理
 import logRouter from './log';
 
 import notFoundRouter from './NotFound';
@@ -13,7 +16,8 @@ export const createRoutes = store => ({
     indexRoute: Home,
     breadcrumbName: '主 页',
     childRoutes: [
-      logRouter(store)
+      logRouter(store),
+      sportsmanRouter(store),
     ],
   },
     loginRoute(store),
